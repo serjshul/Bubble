94600ce2aea2f8339d868ed612f9e223c557a6cc
Sergey Shulgin <shulginsergey0@gmail.com>
2024-09-07 21:47:59 +0300

    `HomeScreenContent` adding

--- app/src/main/java/com/serjshul/bubble/ui/screens/main/home/HomeScreen.kt
+++ app/src/main/java/com/serjshul/bubble/ui/screens/main/home/HomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,14 +14,25 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.serjshul.bubble.data.articles
+import com.serjshul.bubble.model.collections.Article
 import com.serjshul.bubble.ui.theme.md_theme_light_background
-import com.serjshul.bubble.ui.utils.getColor
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
+) {
+    HomeScreenContent(
+        modifier = modifier,
+        banner = viewModel.banner
+    )
+}
+
+@Composable
+fun HomeScreenContent(
+    modifier: Modifier = Modifier,
+    banner: Article
 ) {
     Scaffold(
         modifier = modifier
@@ -44,10 +54,7 @@ fun HomeScreen(
             item {
                 Banner(
                     modifier = Modifier,
-                    title = "Radical Optimism",
-                    description = "Dua Lipa’s star power sounds muffled on her much-anticipated third album, which has many interesting ideas for songs and a surprisingly low hit rate.",
-                    coverUrl = "https://vinyla.com/files/products/40/106/209002/photo-2024-03-15-10-38-20.1280x1280.jpg?c258d668158a54a172e09aa86daa2731",
-                    color = "#055c62".getColor,
+                    article = banner,
                     onReadClick = {},
                     onAddToFavoritesClick = {},
                     onNotInterestedClick = {}
@@ -68,5 +75,7 @@ fun HomeScreen(
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreenContent(
+        banner = articles[0]
+    )
 }
