03e85d77bbd7971662f79afa27b7c9fe838b6a4e
Sergey Shulgin <shulginsergey0@gmail.com>
2024-09-09 14:26:15 +0300

    `SmallCard` editting

--- app/src/main/java/com/serjshul/bubble/ui/components/cards/SmallCard.kt
+++ app/src/main/java/com/serjshul/bubble/ui/components/cards/SmallCard.kt
@@ -12,13 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
