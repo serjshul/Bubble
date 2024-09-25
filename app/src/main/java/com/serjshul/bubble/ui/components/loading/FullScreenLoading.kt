bdcfc567c2570b87c9bd830a8b6c2075d9c35a9a
Sergey Shulgin <shulginsergey0@gmail.com>
2024-09-25 15:59:29 +0300

    `LoadingContent` setup

--- app/src/main/java/com/serjshul/bubble/ui/components/loading/FullScreenLoading.kt
+++ app/src/main/java/com/serjshul/bubble/ui/components/loading/FullScreenLoading.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-private fun FullScreenLoading() {
+fun FullScreenLoading() {
     Box(
         modifier = Modifier
             .fillMaxSize()
--- app/src/main/java/com/serjshul/bubble/ui/components/loading/LoadingContent.kt
+++ app/src/main/java/com/serjshul/bubble/ui/components/loading/LoadingContent.kt
@@ -1,11 +1,18 @@
 package com.serjshul.bubble.ui.components.loading
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 @Composable
-private fun LoadingContent(
+fun LoadingContent(
+    modifier: Modifier = Modifier,
     empty: Boolean,
     emptyContent: @Composable () -> Unit,
     loading: Boolean,
@@ -16,9 +23,28 @@ private fun LoadingContent(
         emptyContent()
     } else {
         SwipeRefresh(
+            modifier = modifier,
             state = rememberSwipeRefreshState(loading),
             onRefresh = onRefresh,
             content = content,
         )
+    }
+}
+
+@Preview
+@Composable
+fun LoadingContentPreview() {
+    LoadingContent(
+        empty = true,
+        emptyContent = { FullScreenLoading() },
+        loading = true,
+        onRefresh = { }
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "Empty and loading"
+            )
+        }
     }
 }
