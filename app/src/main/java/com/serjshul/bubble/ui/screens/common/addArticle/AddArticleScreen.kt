6de03b9cf521b0aaf74d79ee74f1b936c786296c
Sergey Shulgin <shulginsergey0@gmail.com>
2024-10-28 23:25:08 +0300

    `TagsChips` init

--- app/src/main/java/com/serjshul/bubble/ui/screens/common/addArticle/AddArticleScreen.kt
+++ app/src/main/java/com/serjshul/bubble/ui/screens/common/addArticle/AddArticleScreen.kt
@@ -1,21 +1,45 @@
 package com.serjshul.bubble.ui.screens.common.addArticle
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ContextualFlowRow
+import androidx.compose.foundation.layout.ContextualFlowRowOverflow
+import androidx.compose.foundation.layout.ContextualFlowRowOverflowScope
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +50,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.serjshul.bubble.model.collections.User
@@ -34,6 +59,8 @@ import com.serjshul.bubble.ui.components.text.TextInput
 import com.serjshul.bubble.ui.theme.md_theme_background_gradient
 import com.serjshul.bubble.ui.theme.md_theme_transparent_gray
 import com.serjshul.bubble.ui.theme.md_theme_light_onPrimary
+import com.serjshul.bubble.ui.theme.md_theme_light_primary
+import com.serjshul.bubble.ui.theme.md_theme_transparent
 import java.util.Date
 
 @Composable
@@ -67,6 +94,8 @@ fun AddArticleScreenContent(
 ) {
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp.dp
+
+    var tagsExpanded by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier = modifier.fillMaxSize()
@@ -163,16 +192,45 @@ fun AddArticleScreenContent(
                                 fontWeight = FontWeight.Bold,
                                 style = MaterialTheme.typography.bodyMedium
                             )
-                            Text(
-                                modifier = Modifier
-                                    .weight(1f),
-                                text = "tags",
-                                overflow = TextOverflow.Ellipsis,
-                                textAlign = TextAlign.Center,
-                                color = md_theme_light_onPrimary,
-                                style = MaterialTheme.typography.bodyMedium,
-                                maxLines = 4
-                            )
+
+                            AnimatedContent(
+                                modifier = Modifier.weight(1f),
+                                targetState = tagsExpanded,
+                                transitionSpec = {
+                                    fadeIn(animationSpec = tween(150, 150)) togetherWith
+                                            fadeOut(animationSpec = tween(150)) using SizeTransform { initialSize, targetSize ->
+                                                if (targetState) {
+                                                    keyframes {
+                                                        // Expand horizontally first.
+                                                        IntSize(targetSize.width, initialSize.height) at 150
+                                                        durationMillis = 300
+                                                    }
+                                                } else {
+                                                    keyframes {
+                                                        // Shrink vertically first.
+                                                        IntSize(initialSize.width, targetSize.height) at 150
+                                                        durationMillis = 300
+                                                    }
+                                                }
+                                            }
+                                }, label = "size transform"
+                            ) { targetExpanded ->
+                                if (targetExpanded) {
+                                    //Expanded()
+                                } else {
+                                    Text(
+                                        modifier = Modifier
+                                            .align(Alignment.CenterVertically)
+                                            .clickable { tagsExpanded = true },
+                                        text = "tags",
+                                        overflow = TextOverflow.Ellipsis,
+                                        textAlign = TextAlign.Center,
+                                        color = md_theme_light_onPrimary,
+                                        style = MaterialTheme.typography.bodyMedium,
+                                        maxLines = 4
+                                    )
+                                }
+                            }
                         }
                     }
                     Box(
@@ -213,5 +271,81 @@ fun AddArticleScreenContentPreview() {
         onTitleValueChange = { },
         onCreatorValueChange = { },
         onYearValueChange = { }
+    )
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun TagsChips(
+    modifier: Modifier = Modifier,
+    filmsTags: List<String>,
+    booksTags: List<String>
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(15.dp)
+    ) {
+//        Text(
+//            modifier = Modifier.fillMaxWidth(),
+//            text = "Film's tags",
+//            color = md_theme_light_onBackground,
+//            fontWeight = FontWeight.Bold,
+//            style = MaterialTheme.typography.titleLarge,
+//        )
+        val totalCount = booksTags.size
+        var maxLines by remember { mutableStateOf(3) }
+        val moreOrCollapseIndicator = @Composable { scope: ContextualFlowRowOverflowScope ->
+            val remainingItems = totalCount - scope.shownItemCount
+            SuggestionChip(
+                modifier = Modifier.padding(5.dp, 0.dp),
+                label = { Text(text = if (remainingItems == 0) "Less" else "+ $remainingItems") },
+                colors = SuggestionChipDefaults.suggestionChipColors(
+                    containerColor = md_theme_light_primary,
+                    labelColor = md_theme_light_onPrimary
+                ),
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = md_theme_transparent
+                ),
+                onClick = {
+                    maxLines = if (remainingItems == 0) 3 else Int. MAX_VALUE
+                }
+            )
+        }
+        ContextualFlowRow(
+            modifier = Modifier
+                .safeDrawingPadding()
+                .padding(top = 15.dp)
+                .wrapContentHeight(align = Alignment.Top)
+                .verticalScroll(rememberScrollState()),
+            maxLines = maxLines,
+            overflow = ContextualFlowRowOverflow.expandOrCollapseIndicator(
+                minRowsToShowCollapse = 3,
+                expandIndicator = moreOrCollapseIndicator,
+                collapseIndicator = moreOrCollapseIndicator
+            ),
+            itemCount = totalCount
+        ) { index ->
+            var selected by remember { mutableStateOf(false) }
+            FilterChip(
+                modifier = Modifier.padding(5.dp, 0.dp),
+                onClick = { selected = !selected },
+                label = { Text(booksTags[index]) },
+                selected = selected,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TagsChipsPreview() {
+    TagsChips(
+        filmsTags = listOf("Drama", "Mystery", "Thriller", "Fantasy", "Horror", "Comedy", "Romance",
+            "Crime", "Sci-Fi", "Biography"),
+        booksTags = listOf("Fantasy", "Science Fiction", "Dystopian", "Action", "Adventure",
+            "Mystery", "Thriller", "Suspense", "Historical Fiction", "Classics", "Graphic Novel",
+            "Comic Book", "Detective")
     )
 }
