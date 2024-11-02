27b820388c69c3ab7884fc38332e220e5024b1e6
Sergey Shulgin <shulginsergey0@gmail.com>
2024-11-02 19:18:33 +0300

    `AddTextFilledButton` init

--- app/src/main/java/com/serjshul/bubble/ui/components/buttons/TextFilledButton.kt
+++ app/src/main/java/com/serjshul/bubble/ui/components/buttons/TextFilledButton.kt
@@ -2,9 +2,13 @@ package com.serjshul.bubble.ui.components.buttons
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,6 +43,33 @@ fun TextFilledButton(
     }
 }
 
+@Composable
+fun AddTextFilledButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    enabled: Boolean = true,
+    containerColor: Color,
+    contentColor: Color,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        enabled = enabled,
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        )
+    ) {
+        Icon(
+            modifier = Modifier.padding(end = 5.dp),
+            imageVector = Icons.Filled.Add,
+            contentDescription = ""
+        )
+        Text(text)
+    }
+}
+
 @Preview
 @Composable
 fun TextFilledButtonPreview() {
@@ -50,6 +81,24 @@ fun TextFilledButtonPreview() {
         TextFilledButton(
             modifier = Modifier.align(Alignment.Center),
             text = stringResource(id = R.string.button_read),
+            containerColor = md_theme_light_secondary,
+            contentColor = md_theme_light_onSecondary,
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AddTextFilledButtonPreview() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Black)
+    ) {
+        AddTextFilledButton(
+            modifier = Modifier.align(Alignment.Center),
+            text = "Add a background",
             containerColor = md_theme_light_secondary,
             contentColor = md_theme_light_onSecondary,
             onClick = {}
