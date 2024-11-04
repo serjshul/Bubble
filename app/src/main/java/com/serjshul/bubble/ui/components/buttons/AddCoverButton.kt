553e9e796bf3dee246b76fbb759a626874333dc5
Sergey Shulgin <shulginsergey0@gmail.com>
2024-11-04 20:46:06 +0300

    `CoverDialog` and `AddCoverButton` init

---
+++ app/src/main/java/com/serjshul/bubble/ui/components/buttons/AddCoverButton.kt
@@ -0,0 +1,85 @@
+package com.serjshul.bubble.ui.components.buttons
+
+import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.serjshul.bubble.ui.components.media.CoverAsyncImage
+import com.serjshul.bubble.ui.theme.md_theme_dark_gradient
+import com.serjshul.bubble.ui.theme.md_theme_light_onSecondary
+import com.serjshul.bubble.ui.theme.md_theme_light_secondary
+
+@Composable
+fun AddCoverButton(
+    modifier: Modifier = Modifier,
+    width: Dp,
+    height: Dp,
+    coverUri: Uri?,
+    onCoverClick: () -> Unit,
+    onAddCoverClick: () -> Unit
+) {
+    if (coverUri != null) {
+        CoverAsyncImage(
+            modifier = modifier
+                .size(width, height)
+                .clip(RoundedCornerShape(5.dp))
+                .clickable { onCoverClick() },
+            url = coverUri,
+            contentDescription = "Cover URL"
+        )
+    } else {
+        Box(
+            modifier = modifier
+                .size(width, height)
+                .clip(RoundedCornerShape(5.dp))
+                .background(Brush.verticalGradient(md_theme_dark_gradient))
+        ) {
+            AddIconToggleButton(
+                modifier = Modifier.align(Alignment.Center),
+                backgroundColor = md_theme_light_secondary,
+                tint = md_theme_light_onSecondary,
+                onClick = onAddCoverClick
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddCoverButtonWithImagePreview() {
+    val coverWidth = 90.dp
+    val coverHeight = coverWidth * 10 / 19
+
+    AddCoverButton(
+        width = coverWidth,
+        height = coverHeight,
+        coverUri = Uri.EMPTY,
+        onCoverClick = { },
+        onAddCoverClick = { }
+    )
+}
+
+@Preview
+@Composable
+fun AddCoverButtonWithoutImagePreview() {
+    val coverWidth = 90.dp
+    val coverHeight = coverWidth * 10 / 19
+
+    AddCoverButton(
+        width = coverWidth,
+        height = coverHeight,
+        coverUri = null,
+        onCoverClick = { },
+        onAddCoverClick = { }
+    )
+}
---
+++ app/src/main/java/com/serjshul/bubble/ui/components/dialogs/CoverDialog.kt
@@ -0,0 +1,93 @@
+package com.serjshul.bubble.ui.components.dialogs
+
+import android.net.Uri
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.serjshul.bubble.ui.components.buttons.AddCoverButton
+import com.serjshul.bubble.ui.components.buttons.CloseIconToggleButton
+import com.serjshul.bubble.ui.components.buttons.TextFilledButton
+import com.serjshul.bubble.ui.theme.md_theme_light_onPrimary
+import com.serjshul.bubble.ui.theme.md_theme_light_onSecondary
+import com.serjshul.bubble.ui.theme.md_theme_light_primary
+import com.serjshul.bubble.ui.theme.md_theme_light_secondary
+import com.serjshul.bubble.ui.theme.md_theme_transparent_gray
+
+@Composable
+fun CoverDialog(
+    modifier: Modifier = Modifier,
+    coverUri: Uri?,
+    onCoverUriValueChange: (Uri?) -> Unit,
+    onLauncherOpen: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    val coverWidth = configuration.screenWidthDp.dp - 30.dp
+    val coverHeight = coverWidth * 9 / 16
+
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Column(
+            modifier = modifier
+        ) {
+            Box {
+                AddCoverButton(
+                    width = coverWidth,
+                    height = coverHeight,
+                    coverUri = coverUri,
+                    onCoverClick = { /*TODO*/ },
+                    onAddCoverClick = { onLauncherOpen() }
+                )
+                CloseIconToggleButton(
+                    modifier = Modifier.align(Alignment.TopEnd),
+                    backgroundColor = md_theme_transparent_gray,
+                    tint = md_theme_light_onSecondary,
+                    onClick = { onDismissRequest() }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .padding(top = 15.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                TextFilledButton(
+                    modifier = Modifier.width(95.dp),
+                    text = "Remove",
+                    enabled = true,
+                    containerColor = md_theme_light_primary,
+                    contentColor = md_theme_light_onPrimary,
+                    onClick = { onCoverUriValueChange(null) }
+                )
+                TextFilledButton(
+                    modifier = Modifier.padding(start = 15.dp),
+                    text = "Change",
+                    enabled = true,
+                    containerColor = md_theme_light_secondary,
+                    contentColor = md_theme_light_onPrimary,
+                    onClick = { onLauncherOpen() }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CoverDialogPreview() {
+    CoverDialog(
+        coverUri = Uri.EMPTY,
+        onCoverUriValueChange = { },
+        onLauncherOpen = { },
+        onDismissRequest = { }
+    )
+}
